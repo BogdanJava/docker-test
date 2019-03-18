ac07a563058093b18ffd8f8183a95943c73ff42e
bahdan.shyshkin <bogdanshishkin1998@gmail.com>
2019-03-19 00:22:54 +0300

    mongo-init container added

--- src/main/kotlin/by/bogdan/dockerproject/model/User.kt
+++ src/main/kotlin/by/bogdan/dockerproject/model/User.kt
@@ -3,10 +3,12 @@ package by.bogdan.dockerproject.model
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.format.annotation.DateTimeFormat
 import java.util.*
 
 @Document(collection = "users")
 data class User(@Id val _id: ObjectId,
                 val id: Long,
                 val name: String,
-                val birthday: Date)
+                val email: String,
+                @DateTimeFormat(style = "yyyy-MM-dd") val birthday: Date)
