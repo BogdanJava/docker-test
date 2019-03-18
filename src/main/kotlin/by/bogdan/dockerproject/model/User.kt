1852f70bfc7bf9f4fbcc15d50a66e35b94413b62
v-bahdan.s <v-bahdan.s@turvo.com>
2019-03-18 22:51:35 +0300

    changes

--- src/main/kotlin/by/bogdan/dockerproject/model/User.kt
+++ src/main/kotlin/by/bogdan/dockerproject/model/User.kt
@@ -1,9 +1,12 @@
 package by.bogdan.dockerproject.model
 
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 
 @Document(collection = "users")
-data class User(val id: Long,
+data class User(@Id val _id: ObjectId,
+                val id: Long,
                 val name: String,
                 val birthday: Date)
